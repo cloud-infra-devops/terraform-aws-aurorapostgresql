77732ffb2cb9f0763eb00aef41368bd2513e6b42
Pratik Das <Pratik.Das4@cognizant.com>
2026-01-18 00:50:18 -0600

    changed rotation days value

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ module "aurora_postgres_cluster" {
   lambda_subnet_ids                          = ["subnet-0260bb197628ace27", "subnet-0d316885c8257bf12"]
   db_master_username                         = "postgreSQLdbAdmin"
   enable_auto_secrets_rotation               = true
-  rotation_days                              = 90
+  rotation_days                              = 30
   use_existing_kms_key                       = false
   existing_kms_key_arn                       = null
   instance_count                             = 2
