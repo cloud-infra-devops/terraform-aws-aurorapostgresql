12ae147bdf25bafabce2ae8839eec179236d0f65
Pratik Das <Pratik.Das4@cognizant.com>
2026-01-18 12:14:59 -0600

    removed qualifier in lambda secrets invoke permissions

--- aurorapostgreSQL/main.tf
+++ aurorapostgreSQL/main.tf
@@ -638,7 +638,6 @@ resource "aws_lambda_permission" "allow_secretsmanager_invoke" {
   function_name = aws_lambda_function.rotation.function_name
   principal     = "secretsmanager.amazonaws.com"
   source_arn    = aws_secretsmanager_secret.db_master.arn
-  qualifier     = aws_lambda_function.rotation.version
 }
 
 # Rotation using AWS managed single-user rotation Lambda function
